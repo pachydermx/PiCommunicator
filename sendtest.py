01ea0fc2e125f87bfd9a3d77433b70fa8d3a35a0
Ren Jimin <renjimin@gmail.com>
2016-03-07 15:03:45 +0900

    Source Optimized

--- sendtest.py
+++ sendtest.py
@@ -1,8 +1,8 @@
 import time
-import receiver
+import transmit
 
-r = receiver.Transmit("localhost", 8888, False)
-s = receiver.Transmit("localhost", 8888, True)
+r = transmit.Transmit("localhost", 8888, False)
+s = transmit.Transmit("localhost", 8888, True)
 
 r.start()
 time.sleep(1)
